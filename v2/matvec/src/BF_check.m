73af63a681699d4c8e36f4d4813ee0975785f7ee
HaizhaoYang <haizhaoyang@yahoo.com>
2019-05-03 15:22:28 +0800

    update BF_check

--- v2/matvec/src/BF_check.m
+++ v2/matvec/src/BF_check.m
@@ -9,7 +9,7 @@ ext = zeros(NC,1);
 for g=1:NC
     x = floor(rand(1)*N)+1;
     app(g) = u(x);
-    ext(g) = sum(fun(xx(x),kk)*(f(:)));
+    ext(g) = sum(fun(xx(x,:),kk)*(f(:)));
 end
 err = app-ext;
 relerr = norm(err)/norm(ext);
